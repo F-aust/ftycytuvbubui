29fe0d06070240a08167c99c4532eb999d393ad7
F-aust <57711336+F-aust@users.noreply.github.com>
2019-11-15 20:17:31 +0800

    Update gitDemo.java

--- src/main/java/cn/itcst/git/gitDemo.java
+++ src/main/java/cn/itcst/git/gitDemo.java
@@ -4,5 +4,6 @@ public class gitDemo {
     public static void main(String[] args) {
         System.out.println("啦啦啦啦");
         System.out.println("我是卖报的小行家");
+        System.out.println("我是卖报的");
     }
 }
